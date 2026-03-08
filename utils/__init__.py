7059b3cd3c4dbe3f6f0ab9dae6db76d7abb5c712
Exploit Lab <jiaminji24@gmail.com>
2026-03-08 13:20:54 +0100

    Bump version to 1.1.0 and update exports

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 __author__ = 'Exploit Lab'
 __description__ = 'Website Technology Detection Tool'
 
@@ -8,10 +8,10 @@ from .platform_check import check_platform_requirements
 
 __all__ = [
     'display_banner',
-    'print_success', 
+    'print_success',
     'print_error',
     'print_warning',
     'print_info',
     'TechnologyDetector',
-    'check_platform_requirements'
+    'check_platform_requirements',
 ]
